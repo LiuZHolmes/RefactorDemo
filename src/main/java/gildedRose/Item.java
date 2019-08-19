631b1413dbb603fc9a002f62701eb1189098c4de
retr0 <395637649@qq.com>
2019-08-19 19:44:14 +0800

    [refactor]move method to Item

--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -24,4 +24,31 @@ public class Item {
     void increaseQuality() {
         quality = quality < MAX_QUALITY ? quality + 1 : quality;
     }
+    
+    void calQuality() {
+        if (!this.name.equals("Aged Brie")
+                && !this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (this.quality > 0)
+                if (!this.name.equals("Sulfuras, Hand of Ragnaros")) this.quality = this.quality - 1;
+        } else {
+            this.increaseQuality();
+            if (this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (this.sellIn < 11) this.increaseQuality();
+                if (this.sellIn < 6) this.increaseQuality();
+            }
+        }
+
+        if (!this.name.equals("Sulfuras, Hand of Ragnaros")) this.sellIn = this.sellIn - 1;
+
+        if (this.sellIn < 0) {
+            if (!this.name.equals("Aged Brie")) {
+                if (!this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (this.quality > 0) {
+                        if (!this.name.equals("Sulfuras, Hand of Ragnaros"))
+                            this.quality = this.quality - 1;
+                    }
+                } else this.quality = 0;
+            } else this.increaseQuality();
+        }
+    }
 }
