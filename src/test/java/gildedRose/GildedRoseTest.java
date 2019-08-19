a19208e9ef5d564360bb94035b1e8eb3a74bfbb8
retr0 <395637649@qq.com>
2019-08-19 19:13:46 +0800

    [test]should_return_quality_1_when_given_items_name_Sulfuras_Hand_of_Ragnaros_and_sell_in_0_and_quality_1

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -56,12 +56,22 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void should_return_quality_1_when_given_items_name_Sulfuras_Hand_of_Ragnaros_and_sell_in_0_and_quality_0() {
+    public void should_return_quality_0_when_given_items_name_Sulfuras_Hand_of_Ragnaros_and_sell_in_0_and_quality_0() {
         Item[] items = {new Item("Sulfuras, Hand of Ragnaros", 0, 0)};
         GildedRose gildedRose = new GildedRose(items);
 
         gildedRose.updateQuality();
 
         assertEquals(0, items[0].quality);
+    }
+
+    @Test
+    public void should_return_quality_1_when_given_items_name_Sulfuras_Hand_of_Ragnaros_and_sell_in_0_and_quality_1() {
+        Item[] items = {new Item("Sulfuras, Hand of Ragnaros", 0, 1)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(1, items[0].quality);
     }
 }
