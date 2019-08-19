459a9c1cd3f3976d4eae2c0594cce9ce7bdbe855
retr0 <395637649@qq.com>
2019-08-19 19:41:47 +0800

    [refactor]remove braces

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -11,11 +11,8 @@ public class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+                if (items[i].quality > 0)
+                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) items[i].quality = items[i].quality - 1;
             } else {
                 items[i].increaseQuality();
                 if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -24,21 +21,16 @@ public class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) items[i].sellIn = items[i].sellIn - 1;
 
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros"))
                                 items[i].quality = items[i].quality - 1;
-                            }
                         }
-                    } else {
-                        items[i].quality = 0;
-                    }
+                    } else items[i].quality = 0;
                 } else items[i].increaseQuality();
             }
         }
