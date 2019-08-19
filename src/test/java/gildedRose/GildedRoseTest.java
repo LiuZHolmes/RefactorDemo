c55a99516ee647cb8f6de776a11381e474bfd7c2
retr0 <395637649@qq.com>
2019-08-19 18:54:56 +0800

    [test]should_return_quality_50_when_given_items_name_Aged_Brie_and_sell_in_0_and_quality_50

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -7,12 +7,22 @@ import static org.junit.Assert.*;
 public class GildedRoseTest {
     @Test
     public void should_return_quality_2_when_given_items_name_Aged_Brie_and_sell_in_0_and_quality_0() {
-        Item[] items = {new Item("Aged Brie",0,0)};
+        Item[] items = {new Item("Aged Brie", 0, 0)};
         GildedRose gildedRose = new GildedRose(items);
 
         gildedRose.updateQuality();
 
-        assertEquals(2,items[0].quality);
+        assertEquals(2, items[0].quality);
+    }
+
+    @Test
+    public void should_return_quality_50_when_given_items_name_Aged_Brie_and_sell_in_0_and_quality_50() {
+        Item[] items = {new Item("Aged Brie", 0, 50)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(50, items[0].quality);
     }
 
 }
