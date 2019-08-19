e2f30cf93cabaed76e5e052cbff8459ba311d0ac
retr0 <395637649@qq.com>
2019-08-19 18:49:40 +0800

    [test]should_return_quality_2_when_given_items_name_Aged_Brie_and_sell_in_0_and_quality_0

---
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -0,0 +1,18 @@
+package gildedRose;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GildedRoseTest {
+    @Test
+    public void should_return_quality_2_when_given_items_name_Aged_Brie_and_sell_in_0_and_quality_0() {
+        Item[] items = {new Item("Aged Brie",0,0)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(2,items[0].quality);
+    }
+
+}
