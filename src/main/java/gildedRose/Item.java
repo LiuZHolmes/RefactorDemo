27fa66dd5c449f6f18601cf5d4f1a0e3427887bd
retr0 <395637649@qq.com>
2019-08-19 19:59:17 +0800

    [refactor]extract Backstage passes to a TAFKAL80ETC concert situation

--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -32,27 +32,25 @@ public class Item {
             if (this.sellIn < 0) {
                 this.increaseQuality();
             }
-        } else {
-            if (!this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (this.quality > 0)
-                    if (!this.name.equals("Sulfuras, Hand of Ragnaros")) this.quality--;
-            } else {
-                this.increaseQuality();
-                if (this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (this.sellIn < 11) this.increaseQuality();
-                    if (this.sellIn < 6) this.increaseQuality();
-                }
+        } else if (this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            this.increaseQuality();
+            if (this.sellIn < 11) this.increaseQuality();
+            if (this.sellIn < 6) this.increaseQuality();
+            this.sellIn = this.sellIn - 1;
+            if (this.sellIn < 0) {
+                this.quality = 0;
             }
+        } else {
+            if (this.quality > 0)
+                if (!this.name.equals("Sulfuras, Hand of Ragnaros")) this.quality--;
 
             if (!this.name.equals("Sulfuras, Hand of Ragnaros")) this.sellIn = this.sellIn - 1;
 
             if (this.sellIn < 0) {
-                if (!this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (this.quality > 0) {
-                        if (!this.name.equals("Sulfuras, Hand of Ragnaros"))
-                            this.quality--;
-                    }
-                } else this.quality = 0;
+                if (this.quality > 0) {
+                    if (!this.name.equals("Sulfuras, Hand of Ragnaros"))
+                        this.quality--;
+                }
             }
         }
     }
