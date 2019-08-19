e6619d7b9f4809e13ea81532622305c270f0ada3
retr0 <395637649@qq.com>
2019-08-19 20:20:20 +0800

    [fix]extract decreaseQuality & [refactor] remove braces and simplify decrease

--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -3,7 +3,7 @@ package gildedRose;
 public class Item {
 
     final int MAX_QUALITY = 50;
-    final int MIN_QUALITY = 50;
+    final int MIN_QUALITY = 0;
 
     String name;
 
@@ -22,30 +22,33 @@ public class Item {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 
-    private void increaseQuality() {
+    void increaseQuality() {
         quality = quality < MAX_QUALITY ? quality + 1 : quality;
     }
-
-    private void decreaseQuality() {
+    void decreaseQuality() {
         quality = quality > MIN_QUALITY ? quality - 1 : quality;
     }
 
     void calQuality() {
         if (this.name.equals("Aged Brie")) {
             this.increaseQuality();
-            this.sellIn = this.sellIn - 1;
-            if (this.sellIn < 0) this.increaseQuality();
+            if (--this.sellIn < 0) {
+                this.increaseQuality();
+            }
         } else if (this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             this.increaseQuality();
             if (this.sellIn < 11) this.increaseQuality();
             if (this.sellIn < 6) this.increaseQuality();
-            if (--this.sellIn < 0) this.quality = 0;
+            if (--this.sellIn < 0) {
+                this.quality = 0;
+            }
         } else if (this.name.equals("Sulfuras, Hand of Ragnaros")) {
 
         } else {
             decreaseQuality();
-            this.sellIn = this.sellIn - 1;
-            if (this.sellIn < 0) decreaseQuality();
+            if (--this.sellIn < 0) {
+                decreaseQuality();
+            }
         }
     }
 }
