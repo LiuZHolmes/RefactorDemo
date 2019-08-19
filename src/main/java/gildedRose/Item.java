c71b65c462fc6d9c8431fcbbfb9037378354d81f
retr0 <395637649@qq.com>
2019-08-19 20:17:25 +0800

    [refactor]extract decreaseQuality

--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -3,6 +3,7 @@ package gildedRose;
 public class Item {
 
     final int MAX_QUALITY = 50;
+    final int MIN_QUALITY = 50;
 
     String name;
 
@@ -21,37 +22,30 @@ public class Item {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 
-    void increaseQuality() {
+    private void increaseQuality() {
         quality = quality < MAX_QUALITY ? quality + 1 : quality;
+    }
+
+    private void decreaseQuality() {
+        quality = quality > MIN_QUALITY ? quality - 1 : quality;
     }
 
     void calQuality() {
         if (this.name.equals("Aged Brie")) {
             this.increaseQuality();
             this.sellIn = this.sellIn - 1;
-            if (this.sellIn < 0) {
-                this.increaseQuality();
-            }
+            if (this.sellIn < 0) this.increaseQuality();
         } else if (this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             this.increaseQuality();
             if (this.sellIn < 11) this.increaseQuality();
             if (this.sellIn < 6) this.increaseQuality();
-            if (--this.sellIn < 0) {
-                this.quality = 0;
-            }
+            if (--this.sellIn < 0) this.quality = 0;
         } else if (this.name.equals("Sulfuras, Hand of Ragnaros")) {
 
         } else {
-            if (this.quality > 0)
-                this.quality--;
-
+            decreaseQuality();
             this.sellIn = this.sellIn - 1;
-
-            if (this.sellIn < 0) {
-                if (this.quality > 0) {
-                    this.quality--;
-                }
-            }
+            if (this.sellIn < 0) decreaseQuality();
         }
     }
 }
